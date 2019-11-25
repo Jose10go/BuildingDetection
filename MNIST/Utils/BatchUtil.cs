3b5fffc279ff3226a4b3644eab525af38835f01c
Jose10go <Jose10go2017@gmail.com>
2019-11-25 16:18:10 -0500

    memory bugfix

--- MNIST/Utils/BatchUtil.cs
+++ MNIST/Utils/BatchUtil.cs
@@ -215,10 +215,12 @@ namespace CNTKUtil
             var num_indices = end - begin;
             var result = new CNTK.NDArrayView[num_indices];
             var row_index = 0;
+            var temp = new List<float[]>();
             for (var index = begin; index != end; index++)
             {
                 var dataBuffer = source[index];
-                var ndArrayView = new CNTK.NDArrayView(variable.Shape, dataBuffer.Features, CNTK.DeviceDescriptor.CPUDevice, true);
+                temp.Add(dataBuffer.Features);
+                var ndArrayView = new CNTK.NDArrayView(variable.Shape,temp.Last(), CNTK.DeviceDescriptor.CPUDevice, true);
                 result[row_index++] = ndArrayView;
             }
             return CNTK.Value.Create(variable.Shape, result, NetUtil.CurrentDevice, true);
