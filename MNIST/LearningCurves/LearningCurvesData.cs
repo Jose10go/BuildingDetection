b76aa7af16e657a33e146b01d233507809ac2124
Jose10go <Jose10go2017@gmail.com>
2019-12-02 00:56:30 -0500

    Leraning curves data

---
+++ MNIST/LearningCurves/LearningCurvesData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BuildingDetection.LearningCurves
+{
+    public class LearningCurvesData
+    {
+        public const string LearningDataFileName = "learning.json";
+        public List<double> TrainingError { get; set; } 
+        public List<double> TestingError { get; set; }
+
+        public static LearningCurvesData LoadFrom(string Path) 
+        {
+            var anonimous = new { TrainingError = new List<double>(), TestingError = new List<double>() };
+            if (File.Exists(Path))
+            {
+                var str = File.ReadAllText(Path);
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<LearningCurvesData>(str);
+            }
+            if (Directory.Exists(Path))
+            {
+                var str = File.ReadAllText(System.IO.Path.Combine(Path, LearningDataFileName));
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<LearningCurvesData>(str);
+            }
+            return null;
+        }
+    }
+}
