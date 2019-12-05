258e4e988469e97b3aefe7be13712a7be8ad357e
Jose10go <Jose10go2017@gmail.com>
2019-12-05 15:40:18 -0500

    Add Yolo Loss and Error funtion from paper

--- MNIST/Yolo/YOLO.cs
+++ MNIST/Yolo/YOLO.cs
@@ -69,12 +69,138 @@ namespace BuildingDetection.Yolo
 
         public static Function GetYoloLossFunction(this Function network) 
         {
-            return NetUtil.MeanSquaredError(network.Output, labels); ;
+            float coordinatesLambda = 5f;
+            float noObjectLambda = .5f;
+
+            // we create a new function with the same dimension as the prediction variable. 
+            // This new "delta" function (see after the for loop) contains all errors which are calculated by the Yolo v2 loss function. See here: https://pjreddie.com/media/files/papers/yolo_1.pdf
+            // For example: 
+            //  When a particular cell value in the prediction Varibale contain the dx coordinate, 
+            //  then this cell in the "delta" function will contain the squared x-error (when there was an object detection for that anchor box)
+
+            VariableVector deltaChannels = new VariableVector();
+            for (int i = 0; i < B; i++)
+            {
+                int anchorOffset = 5 * i;
+
+                // objectnessTruth is the "1^(obj)" from loss function in the paper
+                Function objectnessTruth = CNTKLib.Slice(labels, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset + 4 }, new IntVector() { anchorOffset + 5 });
+                NDShape singleChannelShape = objectnessTruth.Output.Shape.SubShape(0, 3);
+
+                // noobjectnessTruth is the "1^(noobj)" from loss function in the paper
+                Parameter onesChannel = new Parameter(singleChannelShape, DataType.Float, CNTKLib.ConstantInitializer(1f), DeviceDescriptor.CPUDevice);
+                Function noobjectnessTruth = CNTKLib.Minus(onesChannel, objectnessTruth);
+
+                // coordinatesLambdaChannel is the LAMBDA(coord) from the loss function in the paper
+                Parameter coordinatesLambdaChannel = new Parameter(singleChannelShape, DataType.Float, CNTKLib.ConstantInitializer(coordinatesLambda), DeviceDescriptor.CPUDevice);
+
+                // loss due to differences in centers
+                Function centerPrediction = CNTKLib.Slice(network.Output, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset }, new IntVector() { anchorOffset + 2 });
+                Function centerTruth = CNTKLib.Slice(labels, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset }, new IntVector() { anchorOffset + 2 });
+                Function centerDifference = CNTKLib.Minus(centerPrediction, centerTruth);
+                Function centerLoss = CNTKLib.ElementTimes(coordinatesLambdaChannel, CNTKLib.ElementTimes(objectnessTruth, CNTKLib.Square(centerDifference)));
+                deltaChannels.Add(centerLoss); // Add 1 Function of 2 channels to the vector
+
+                // loss due to differences in width/height
+                Function dimensionPrediction = CNTKLib.Slice(network.Output, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset + 2 }, new IntVector() { anchorOffset + 4 });
+                Function dimensionTruth = CNTKLib.Slice(labels, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset + 2 }, new IntVector() { anchorOffset + 4 });
+                Function dimensionSqrtDifference = CNTKLib.Minus(CNTKLib.Sqrt(dimensionPrediction), CNTKLib.Sqrt(dimensionTruth));
+                Function dimensionLoss = CNTKLib.ElementTimes(coordinatesLambdaChannel, CNTKLib.ElementTimes(objectnessTruth, CNTKLib.Square(dimensionSqrtDifference)));
+                deltaChannels.Add(dimensionLoss); // Add 1 Function of 2 channels to the vector
+
+                // noObjectLambdaChannel is the LAMBDA(noobj) from the loss function in the paper
+                Parameter noObjectLambdaChannel = new Parameter(singleChannelShape, DataType.Float, CNTKLib.ConstantInitializer(noObjectLambda), DeviceDescriptor.CPUDevice);
+                // objectnessWeight is the combination of 1^(obj) + LAMBDA(noobj) * 1^(noobj)
+                Function objectnessWeight = CNTKLib.Plus(objectnessTruth, CNTKLib.ElementTimes(noObjectLambdaChannel, noobjectnessTruth)); // todo this could potentially be wrong as the documentation describes that this is an element wise BINARY addition
+
+                // loss due to difference in objectness
+                Function objectnessPrediction = CNTKLib.Slice(network.Output, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset + 4 }, new IntVector() { anchorOffset + 5 });
+                Function objectnessDifference = CNTKLib.Minus(objectnessPrediction, objectnessTruth);
+                Function objectnessLoss = CNTKLib.ElementTimes(objectnessWeight, CNTKLib.Square(objectnessDifference));
+                deltaChannels.Add(objectnessLoss); // Add 1 Function of 1 channels to the vector
+
+                // loss due to difference in class predictions
+                Function classesPrediction = CNTKLib.Slice(network.Output, new AxisVector() { new Axis(2) }, new IntVector() { 5*B }, new IntVector() { 5*B+C });
+                Function classesTruth = CNTKLib.Slice(labels, new AxisVector() { new Axis(2) }, new IntVector() { 5 * B }, new IntVector() { 5 * B + C });
+                Function classesDifference = CNTKLib.Minus(classesPrediction, classesTruth);
+                Function classesLoss = CNTKLib.ElementTimes(objectnessTruth, CNTKLib.Square(classesDifference));
+                deltaChannels.Add(classesLoss); // Add 1 Function of "classes" channels to the vector
+            }
+
+            // As described above the delta function contains all losses
+            Function delta = CNTKLib.Splice(deltaChannels, new Axis(2));
+
+            ////the loss Function can't be a multidimensional tensor, then reduce the dimension by taking the sum
+            Function totalSum = CNTKLib.ReduceSum(delta, new AxisVector() { new Axis(2), new Axis(1), new Axis(0) });
+            return CNTKLib.Reshape(totalSum, new int[] { 1 });
+
         }
 
-        public static Function GetYoloErrorFunction(this Function network)
+        public static Function GetYoloErrorFunction(this Function network,float threshold=0.3f)
         {
-            return NetUtil.MeanAbsoluteError(network.Output, labels);
+            var prediction = network.Output;
+            // thresholdChannel is a one channel parameter filled with the threshold value
+            NDShape singleChannelShape = new int[] { prediction.Shape[0], prediction.Shape[1], 1 };
+            Parameter thresholdChannel = new Parameter(singleChannelShape, DataType.Float, CNTKLib.ConstantInitializer(threshold), DeviceDescriptor.CPUDevice);
+            Parameter onesChannel = new Parameter(singleChannelShape, DataType.Float, CNTKLib.ConstantInitializer(1f), DeviceDescriptor.CPUDevice);
+
+            VariableVector truePredictionChannels = new VariableVector();
+            VariableVector falsePredictionChannels = new VariableVector();
+            for (int i = 0; i < B; i++)
+            {
+                int anchorOffset = 5  * i;
+
+                Function objectnessTruth = CNTKLib.Slice(labels, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset + 4 }, new IntVector() { anchorOffset + 5 });
+                Function objectnessPrediction = CNTKLib.Slice(prediction, new AxisVector() { new Axis(2) }, new IntVector() { anchorOffset + 4 }, new IntVector() { anchorOffset + 5 });
+                objectnessPrediction = CNTKLib.GreaterEqual(objectnessPrediction, thresholdChannel); // convert to 0 where the value is below threshold, and 1 if it is above threshold
+
+                Function noObjectnessTruth = CNTKLib.Minus(onesChannel, objectnessTruth);
+                Function noObjectnessPrediction = CNTKLib.Minus(onesChannel, objectnessPrediction);
+
+                // todo class prediction should be set to false when it is below threshold
+                Function classesTruth = CNTKLib.Slice(labels, new AxisVector() { new Axis(2) }, new IntVector() { 5 * B }, new IntVector() { 5*B + C});
+                classesTruth = CNTKLib.Argmax(classesTruth, new Axis(2)); // Reduce the tensor to one channel; filled with the index of the maximum value (= the index of the class)
+                Function classesPrediction = CNTKLib.Slice(prediction, new AxisVector() { new Axis(2) }, new IntVector() { 5*B }, new IntVector() { 5*B + C});
+                classesPrediction = CNTKLib.Argmax(classesPrediction, new Axis(2)); // Reduce the tensor to one channel; filled with the index of the maximum value (= the index of the class)
+
+                // True Positives (tensor contains 1 if yes, 0 otherwise) 
+                Function truePositives = CNTKLib.ElementTimes(objectnessTruth, objectnessPrediction); // should detect an object, and detects an object
+                truePositives = CNTKLib.ElementTimes(truePositives, CNTKLib.Equal(classesTruth, classesPrediction)); // and should detecect the same class
+
+                // True Negatives (tensor contains 1 if yes, 0 otherwise)
+                Function trueNegatives = CNTKLib.ElementTimes(noObjectnessTruth, noObjectnessPrediction); // should detect no object, and detects no object
+
+                // False Negatives (tensor contains 1 if yes, 0 otherwise)
+                Function falseNegatives = CNTKLib.ElementTimes(objectnessTruth, noObjectnessPrediction); // should detect an object, but detects no object
+
+                // False Positives (tensor contains 1 if yes, 0 otherwise)
+                Function falsePositives = CNTKLib.ElementTimes(noObjectnessTruth, objectnessPrediction); // should detect no object, but detects an object
+
+                // Missclassification (tensor contains 1 if yes, 0 otherwise)
+                Function missclassifications = CNTKLib.ElementTimes(objectnessTruth, objectnessPrediction); // should detect an object, and detects an object
+                missclassifications = CNTKLib.ElementTimes(missclassifications, CNTKLib.NotEqual(classesTruth, classesPrediction)); // but detects a different class
+
+                truePredictionChannels.Add(truePositives);
+                truePredictionChannels.Add(trueNegatives);
+
+                falsePredictionChannels.Add(falseNegatives);
+                falsePredictionChannels.Add(falsePositives);
+                falsePredictionChannels.Add(missclassifications);
+            }
+
+            Function allGoodPredictions = CNTKLib.Splice(truePredictionChannels, new Axis(2));
+            Function allbadPredictions = CNTKLib.Splice(falsePredictionChannels, new Axis(2));
+
+            // sum the variable vector
+            Function goodPredictionSum = CNTKLib.ReduceSum(allGoodPredictions, new AxisVector() { new Axis(2), new Axis(1), new Axis(0) });
+            Function badPredictionSum = CNTKLib.ReduceSum(allbadPredictions, new AxisVector() { new Axis(2), new Axis(1), new Axis(0) });
+
+            // fail safe: The value of goodPredictionSum isn't allowed to be 0, because the division at the end would fail.
+            Parameter one = new Parameter(new int[] { 1, 1, 1 }, DataType.Float, CNTKLib.ConstantInitializer(1f), DeviceDescriptor.CPUDevice);
+            goodPredictionSum = CNTKLib.ElementMax(goodPredictionSum, one, "Fail safe");
+
+            // device the summations (Take into account the case where there are 0 true predictions
+            return CNTKLib.ElementDivide(badPredictionSum, goodPredictionSum);
         }
 
         private static double GetLearningRate(int epoch,int maxEpochs)
