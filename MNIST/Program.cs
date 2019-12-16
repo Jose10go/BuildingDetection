b6078c1793146e9efa91ecb656bdab0edaeff499
Tessy <Tessy@DESKTOP-BMIDUFP>
2019-12-15 23:36:20 -0500

    Programs Exmples

--- MNIST/Program.cs
+++ MNIST/Program.cs
@@ -1,48 +1,46 @@
-﻿using BuildingDetection.LearningCurves;
+﻿using BuildingDetection.TinyYoloV2;
 using BuildingDetection.Utils;
 using BuildingDetection.Yolo;
 using CNTK;
 using CNTKUtil;
 using MNIST.Utils;
-using OxyPlot;
-using OxyPlot.Series;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using static BuildingDetection.Yolo.YOLO;
-
 namespace MNIST
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            //Examples
             //BuildAndTrainYOLO();
             //TestDrawer();
             //TestDrawLearningCurve();
             //TestDrawLearningCurve2();
-            EvaluateModel();
+            //EvaluateModel();
+            //TransferLearning();
+        }
+
+        private static void TransferLearning()
+        {
+            var (training, testing) = LoadData();
+
+            //build network
+            var yolo2 = TinyYOLOV2.CreateTransferLearningModel<YOLO>(@"../../../../TinyYoloV2/yolo2.onnx","activation7");
+            Console.WriteLine("Model architecture:");
+            Console.WriteLine(yolo2.Network.ToSummary());
+ 
+            yolo2.TrainMinibatchSource(training, testing, "yolo2", 100, 1);
         }
 
         private static void EvaluateModel()
         {
-            var (network,_) = LoadModel("model9.model",ModelFormat.CNTKv2);
-            var (evaluationData, _) = LoadData();
+            var yolo = Model.LoadModel<YOLO>("model9.model",ModelFormat.CNTKv2);
+            var (evaluationData, _) = LoadData(1);
             foreach (var item in evaluationData)
-            {
-                var inputs = new Dictionary<Variable, Value>();
-                inputs.Add(network.Arguments.First(),new Value(new NDArrayView(new NDShape(new SizeTVector() {(uint)W,(uint)H,3}),item.Features,DeviceDescriptor.CPUDevice)));
-                var outputs = new Dictionary<Variable, Value>() { {network.Output,null} };
-                network.Evaluate(inputs,outputs,DeviceDescriptor.CPUDevice);
-                var boundingboxes = YOLOOutputParser.ParseOutputs(outputs[network.Output].GetDenseData<float>(network.Output)[0].ToArray());
-                boundingboxes = YOLOOutputParser.FilterBoundingBoxes(boundingboxes, 3, 0.1f);
-                var dir = Path.GetDirectoryName(item.Reference);
-                var imgname = Path.GetFileName(item.Reference);
-                YoloBoundingBox.DrawBoundingBox(dir, Path.Combine(dir, "out"), imgname, boundingboxes);
-            }
+                yolo.Evaluate(item);
 
         }
 
@@ -77,216 +75,22 @@ namespace MNIST
             var (training, testing) = LoadData();
 
             //build network
-            var network = BuildYoloDNN();
+            var yolo = new YOLO();
             Console.WriteLine("Model architecture:");
-            Console.WriteLine(network.ToSummary());
+            Console.WriteLine(yolo.Network.ToSummary());
 
-            // set up the loss function and the error function
-            var lossFunc = network.GetYoloLossFunction();
-            var errorFunc = network.GetYoloErrorFunction();
-            TrainMinibatchSource(network, training, testing, lossFunc, errorFunc, "yolo", 10, 10);
+            yolo.TrainMinibatchSource(training, testing, "yolo", 10, 3);
         }
 
         static (IData[] training,IData[] testing) LoadData(double percent = 0.8) 
         {
-            //// load training and testing data
             Console.WriteLine("Loading data....");
-            var l = ReadAnnotationImages.ReadFromDirectory(@"C:\Users\Jose10go\Downloads\dataset", 416, 416);
+            var l = ReadAnnotationImages.ReadFromDirectory(@"../../../../dataset",TinyYOLOV2.W,TinyYOLOV2.H);
             var split = (int)(percent * l.Count);
             var training = l.Take(split).ToArray();
             var testing = l.Skip(split).ToArray();
             return (training, testing);
         }
 
-        static void Train(Function network,IData[] training,IData[] testing,Function lossFunc,Function errorFunc,string outputPath,int maxEpochs/*135*/, int batchSize/*64*/,bool autoSave=true,int autoSaveStep=2) 
-        {
-            // train the model
-            Console.WriteLine("Epoch\tTrain\tTrain\tTest");
-            Console.WriteLine("\tLoss\tError\tError");
-            Console.WriteLine("-----------------------------");
-
-            var loss = new double[maxEpochs];
-            var trainingError = new List<double>(maxEpochs);
-            var testingError = new List<double>(maxEpochs);
-
-            for (int epoch = 0; epoch < maxEpochs; epoch++)
-            {
-                var learner = network.GetYoloLearner(epoch,maxEpochs);
-                var trainer = network.GetTrainer(learner, lossFunc, errorFunc);
-                var evaluator = network.GetEvaluator(errorFunc);
-                // train one epoch on batches
-                loss[epoch] = 0.0;
-                trainingError[epoch] = 0.0;
-                var batchCount = 0;
-                training.Index().Shuffle().Batch(batchSize, (indices, begin, end) =>
-                {
-                    // get the current batch
-                    var featureBatch = features.GetFeaturesBatch(training, indices, begin, end);
-                    var labelBatch = labels.GetLabelsBatch(training, indices, begin, end);
-
-                    // train the network on the batch
-                    var result = trainer.TrainBatch(
-                                        new[] {
-                                            (features, featureBatch),
-                                            (labels,  labelBatch)
-                                        },
-                                        false);
-                    loss[epoch] += result.Loss;
-                    trainingError[epoch] += result.Evaluation;
-                    batchCount++;
-                });
-
-                // show results
-                loss[epoch] /= batchCount;
-                trainingError[epoch] /= batchCount;
-                Console.Write($"{epoch}\t{loss[epoch]:F3}\t{trainingError[epoch]:F3}\t");
-
-                // test one epoch on batches
-                testingError[epoch] = 0.0;
-                batchCount = 0;
-                testing.Batch(batchSize, (data, begin, end) =>
-                {
-                    // get the current batch for testing
-                    var featureBatch = features.GetFeaturesBatch(testing, begin, end);
-                    var labelBatch = labels.GetLabelsBatch(testing, begin, end);
-
-                    // test the network on the batch
-                    testingError[epoch] += evaluator.TestBatch(
-                        new[] {
-                            (features, featureBatch),
-                            (labels,  labelBatch)
-                        }
-                    );
-                    batchCount++;
-                });
-                testingError[epoch] /= batchCount;
-                Console.WriteLine($"{testingError[epoch]:F3}");
-                if (epoch % autoSaveStep==0)
-                    SaveModel(network,trainingError,testingError, outputPath);
-            }
-
-            // show final results
-            var finalError = testingError.Last();
-            Console.WriteLine();
-            Console.WriteLine($"Final test error: {finalError:0.00}");
-            Console.WriteLine($"Final test accuracy: {1 - finalError:0.00}");
-            SaveModel(network,trainingError,testingError, outputPath);
-        }
-
-        private static void SaveModel(Function network, List<double> trainingError, List<double> testingError, string outputPath)
-        {
-            Directory.CreateDirectory(outputPath);
-            network.Save($"{outputPath}/model.model");
-            File.WriteAllText($"{outputPath}/learning.json",Newtonsoft.Json.JsonConvert.SerializeObject(new {TrainingError=trainingError,TestingError=testingError}));
-        }
-        
-        private static (Function network, LearningCurvesData learningData) LoadModel(string path,ModelFormat format)
-        {
-            var network=Function.Load(path,DeviceDescriptor.CPUDevice,format);
-            var dirPath = Path.GetDirectoryName(path);
-            var data = LearningCurvesData.LoadFrom(dirPath);
-            return (network,data);
-        }
-
-        private static Function CreateTransferLearningModel(string baseModelFile,string hiddenNodeName,ParameterCloningMethod parameterCloningMethod=ParameterCloningMethod.Freeze)
-        {
-            Function baseModel = Function.Load(baseModelFile, DeviceDescriptor.CPUDevice);
-            Function lastNode = baseModel.FindByName(hiddenNodeName);
-
-            // Clone the desired layers with fixed weights
-            Function clonedLayer = CNTKLib.AsComposite(lastNode).Clone(parameterCloningMethod);
-            return clonedLayer;
-        }
-
-        private static MinibatchSource CreateMinibatchSource(string image_map_file,string label_map_file)
-        {
-            List<CNTKDictionary> transforms = new List<CNTKDictionary>
-            {
-                CNTKLib.ReaderScale(W,H,3, "linear")
-            };
-            var ctfDeserializer = CNTKLib.CTFDeserializer(label_map_file, new StreamConfigurationVector() { new StreamConfiguration("boundingbox",S*S*(5*B+C))});
-            var imageDeserializer = CNTKLib.ImageDeserializer(image_map_file, "label", 1, "image", transforms);
-            MinibatchSourceConfig config = new MinibatchSourceConfig(new List<CNTKDictionary> { imageDeserializer, ctfDeserializer });
-
-            return CNTKLib.CreateCompositeMinibatchSource(config);
-        }
-
-        static void TrainMinibatchSource(Function network, IData[] training, IData[] testing, Function lossFunc, Function errorFunc, string outputPath, int maxEpochs/*135*/, int batchSize/*64*/, bool autoSave = true, int autoSaveStep = 2)
-        {
-            // train the model
-            Console.WriteLine("Epoch\tTrain\tTrain\tTest");
-            Console.WriteLine("\tLoss\tError\tError");
-            Console.WriteLine("-----------------------------");
-
-            var loss = Enumerable.Repeat(0d,maxEpochs).ToList();
-            var trainingError = Enumerable.Repeat(0d,maxEpochs).ToList();
-            var testingError = Enumerable.Repeat(0d,maxEpochs).ToList();
-            var (imageMap, dataMap) = CreateMapFiles(training);
-
-            for (int epoch = 0; epoch < maxEpochs; epoch++)
-            {
-                var learner = network.GetYoloLearner(epoch, maxEpochs);
-                var trainer = network.GetTrainer(learner, lossFunc, errorFunc);
-                var evaluator = network.GetEvaluator(errorFunc);
-                
-                var source = CreateMinibatchSource(imageMap, dataMap);
-                var featureStreamInfo = source.StreamInfo("image");
-                var labelStreamInfo = source.StreamInfo("boundingbox");
-                int batchCant = 10;
-                for (int batchCount = 0; batchCount < batchCant; batchCount++)
-                {
-                    var batch = source.GetNextMinibatch((uint)batchSize,DeviceDescriptor.CPUDevice);
-                    // train the network on the batch
-                    var result = trainer.TrainMinibatch(new Dictionary<Variable, MinibatchData>() {
-                                                                { features, batch[featureStreamInfo]},
-                                                                { labels, batch[labelStreamInfo]}},
-                                                                DeviceDescriptor.CPUDevice);
-
-                    loss[epoch] += trainer.PreviousMinibatchLossAverage();
-                    trainingError[epoch] += trainer.PreviousMinibatchEvaluationAverage();
-                }
-                // show results
-                loss[epoch] /= batchCant;
-                trainingError[epoch] /= batchCant;
-                Console.Write($"{epoch}\t{loss[epoch]:F3}\t{trainingError[epoch]:F3}\t");
-
-                for (int batchCount = 0; batchCount < batchCant; batchCount++)
-                {
-                    var batch = source.GetNextMinibatch((uint)batchSize, DeviceDescriptor.CPUDevice);
-                    // test the network on the batch
-                    testingError[epoch] += evaluator.TestMinibatch(new UnorderedMapVariableMinibatchData() {
-                                                                    { features, batch[featureStreamInfo]},
-                                                                    { labels, batch[labelStreamInfo]}},
-                                                                    DeviceDescriptor.CPUDevice); ;
-                }
-                testingError[epoch] /= batchCant;
-                Console.WriteLine($"{testingError[epoch]:F3}");
-                if (epoch % autoSaveStep == 0)
-                    SaveModel(network, trainingError, testingError, outputPath);
-            }
-
-            // show final results
-            var finalError = testingError.Last();
-            Console.WriteLine();
-            Console.WriteLine($"Final test error: {finalError:0.00}");
-            Console.WriteLine($"Final test accuracy: {1 - finalError:0.00}");
-            SaveModel(network, trainingError, testingError, outputPath);
-        }
-
-        private static (string imagemap,string labelmap) CreateMapFiles(IData[] data)
-        {
-            string imagemap = "imagesmap.txt";
-            string labelmap = "labelsmap.txt";
-            StringBuilder images = new StringBuilder();
-            StringBuilder labels = new StringBuilder();
-            foreach (var item in data)
-            {
-                images.AppendLine(item.Reference+"\t 0");
-                labels.AppendLine("|boundingbox "+ string.Join(' ',item.Labels));
-            }
-            File.WriteAllText(imagemap,images.ToString());
-            File.WriteAllText(labelmap,labels.ToString());
-            return (imagemap, labelmap);
-        }
     }
 }
